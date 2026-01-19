45fd3ecd9a44971a57426ceba7093d1e0daef5e0
Jasper Gerigk <jasper.gerigk@mail.utoronto.ca>
2026-01-19 16:01:48 -0500

    Made finetune pass through arguments

--- policy/pi0/finetune.sh
+++ policy/pi0/finetune.sh
@@ -4,4 +4,4 @@ gpu_use=$3
 
 export CUDA_VISIBLE_DEVICES=$gpu_use
 echo $CUDA_VISIBLE_DEVICES
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py $train_config_name --exp-name=$model_name --overwrite
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py $train_config_name --exp-name=$model_name --overwrite "${@:4}"
